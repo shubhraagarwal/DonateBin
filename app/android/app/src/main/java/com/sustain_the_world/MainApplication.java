f2e2f43d77e68b2af8fbd855bef4cbd99d0ff90e
Sumrit Grover <sumritgrover1@gmail.com>
2021-11-20 12:56:04 +0530

    camera included

--- app/android/app/src/main/java/com/sustain_the_world/MainApplication.java
+++ app/android/app/src/main/java/com/sustain_the_world/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
